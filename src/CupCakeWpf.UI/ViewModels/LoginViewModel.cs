8a047bf10d9d4429d0bfedc057f2794eeaff1179
shelbincormsquare <shelbin@cormsquare.com>
2018-10-28 11:59:27 +0530

    d

---
+++ src/CupCakeWpf.UI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CupCakeWpf.UI.ViewModels.Interfaces;
+using Prism.Commands;
+using System;
+using System.Windows.Input;
+
+namespace CupCakeWpf.UI.ViewModels
+{
+    public class LoginViewModel : ViewModelBase, ILoginViewModel
+    {
+        public ICommand LoginCommand { get; }
+
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; OnPropertyChanged(); }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; OnPropertyChanged(); }
+        }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new DelegateCommand(OnLoginExecute, OnLoginCanExecute);
+        }
+
+        private bool OnLoginCanExecute()
+        {
+            return true;
+        }
+
+        private void OnLoginExecute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
