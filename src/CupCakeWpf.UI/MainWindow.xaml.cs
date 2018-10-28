3607395fb9eed988427462bd44f2d1b098fbfba3
shelbincormsquare <shelbin@cormsquare.com>
2018-10-28 11:33:21 +0530

    Add project files.

---
+++ src/CupCakeWpf.UI/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace CupCakeWpf.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
