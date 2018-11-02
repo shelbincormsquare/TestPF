ce5ea9ea995a39257f2917c40d5867e8a9666ce8
shelbincormsquare <shelbin@cormsquare.com>
2018-11-02 07:22:44 +0530

    monor

---
+++ src/CupCakeWpf.UI/ViewModels/InvoiceEditorViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace CupCakeWpf.UI.ViewModels
+{
+    public class InvoiceEditorViewModel : ViewModelBase
+    {
+
+    }
+
+
+    public class Invoice
+    {
+        public string Code { get; set; }
+        public string CustomerName { get; set; }
+        public decimal TotalAmount { get; set; }
+        public decimal TotalTax { get; set; }
+        public List<InvoiceItem> InvoiceItems { get; set; }
+    }
+
+    public class InvoiceItem
+    {
+        public string Name { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TaxRate { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+
+    public class DataService
+    {
+        public static void GetInvoices()
+        {
+            var invoice = new Invoice()
+            {
+                Code = "Code1",
+                CustomerName = "Customer1",
+                TotalAmount = 21822,
+                TotalTax = 221,
+                InvoiceItems = new List<InvoiceItem>()
+                {
+                    new InvoiceItem(){ Name ="Name1", Quantity =10, TaxRate =8, UnitPrice = 22, TotalAmount = 2200 },
+                    new InvoiceItem(){ Name ="Name1", Quantity =10, TaxRate =8, UnitPrice = 22, TotalAmount = 2200 },
+                    new InvoiceItem(){ Name ="Name1", Quantity =10, TaxRate =8, UnitPrice = 22, TotalAmount = 2200 }
+                }
+            };
+        }
+    }
+}
